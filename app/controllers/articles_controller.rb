a27e20c302cbdd61fcd193a43680a523b89d63b4
Deng <donggdeng@gmail.com>
2021-12-13 05:27:04 +0800

    add a simple search articles by title function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,6 +56,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def search
+    @articles = Article.search_by_title(params[:q])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    def self.search_by_title(search_term)
+        return [] if search_term.blank?
+        where("title ILIKE ?", "%#{search_term}%").order("created_at DESC")
+    end
 end
