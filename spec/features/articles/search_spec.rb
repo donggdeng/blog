114513d3b53001c9353990852aff4b6757b10532
Deng <donggdeng@gmail.com>
2021-12-14 02:36:05 +0800

    add feature tests: view articles and update one, search article by title

---
+++ spec/features/articles/search_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature "Searching articles" do
+    background do
+      User.create(email: 'user@example.com', password: 'caplin')
+      Article.create(title: "Title One", content: "Content One")
+      Article.create(title: "Title One Plus", content: "Content One Plus")
+      Article.create(title: "Title Two", content: "Content Two")
+      Article.create(title: "Title Three", content: "Content Three")
+
+      visit '/'
+      within("#session") do
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'caplin'
+      end
+      click_button 'Log in'
+    end
+  
+    scenario "Login and successfully update article" do
+        expect(page).to have_content "One"
+
+        fill_in 'Search', with: "one"
+        click_button 'Search'
+
+        expect(page).to have_content "Title One"
+        expect(page).to have_content "Title One Plus"
+        expect(page).to have_no_content "Two"
+        expect(page).to have_no_content "Three"
+
+        fill_in 'Search', with: 'title'
+        click_button 'Search'
+
+        expect(page).to have_content "Title One"
+        expect(page).to have_content "Title One Plus"
+        expect(page).to have_content "Title Two"
+        expect(page).to have_content "Title Three"
+    end
+end
---
+++ spec/features/articles/update_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature "Updating article" do
+    background do
+      User.create(email: 'user@example.com', password: 'caplin')
+      Article.create(title: "Title One", content: "Content One")
+
+      visit '/'
+      within("#session") do
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'caplin'
+      end
+      click_button 'Log in'
+    end
+  
+    scenario "Login and successfully update article" do
+      within(".articles") do
+        click_link 'Title One'
+      end
+
+      expect(page).to have_content "Edit"
+      expect(page).to have_content "Content One"
+
+      within(".article") do
+        click_link "Edit"
+      end
+
+      expect(page).to have_content "Editing Article"
+      
+      fill_in "Title", with: "New Title"
+      fill_in "Content", with: "New Content"
+
+      click_button "Update Article"
+
+      expect(page).to have_content "success"
+      expect(page).to have_content "New Title"
+      expect(page).to have_content "New Content"
+
+    end
+end
