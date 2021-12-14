25829083c67ec378c6d7259c140a0a2f174c4c4e
Deng <donggdeng@gmail.com>
2021-12-15 00:44:09 +0800

    make search method available for author

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,9 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
-    def self.search_by_title(search_term)
+    def self.search_by_title_or_author(search_term)
         return [] if search_term.blank?
-        where("title ILIKE ?", "%#{search_term}%").order("created_at DESC")
+        where("title ILIKE :search or author ILIKE :search", search: "%#{search_term}%").order("created_at DESC")
     end
 
     def increase_views_count
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,30 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   before do
-    Article.create(title: "One", content: "content one")
-    Article.create(title: "Two", content: "content two")
-    Article.create(title: "Three", content: "content three")
+    Article.create(title: "One", author: "Lucy", content: "content one")
+    Article.create(title: "Two", author: "Jimmy", content: "content two")
+    Article.create(title: "Three", author: "Lily", content: "content three")
+    Article.create(title: "Four", author: "Lone", content: "content four")
   end
-  context 'search by title' do
 
-    it 'should return empty array if title is nil' do
-      title = nil
-      articles = Article.search_by_title title
+  context 'search by title or author' do
+
+    it 'should return empty array if given term is nil' do
+      keyword = nil
+      articles = Article.search_by_title_or_author keyword
       expect(articles).to eq([])
     end
 
-    it 'should return empty array if title is empty' do
-      title = ""
-      articles = Article.search_by_title title
+    it 'should return empty array if given term is empty' do
+      keyword = ""
+      articles = Article.search_by_title_or_author keyword
       expect(articles).to eq([])
     end
 
     it 'should return by order with created_at DESC' do
-      title = "T"
-      articles = Article.search_by_title title
+      keyword = "T"
+      articles = Article.search_by_title_or_author keyword
       expect(articles.size).to eq(2)
       expect(articles.first.title).to eq("Three")
       expect(articles.second.title).to eq("Two")
+    end
+
+    it 'should return all items whose title or author matched given term' do
+      keyword = "one"
+      articles = Article.search_by_title_or_author keyword
+      expect(articles.size).to eq(2)
+      expect(articles.first.author).to eq('Lone')
+      expect(articles.second.title).to eq('One')
     end
 
   end
